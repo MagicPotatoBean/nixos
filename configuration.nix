6ab6eb45a6e06644bf2db68a7ff56c2e8daa4e7c
Zoe Soutter <zoe@soutter.com>
2024-06-26 14:32:03 +0100

    209 current  2024-06-26 14:31:53  24.11pre643242.2893f56de080  6.6.35                          *

--- configuration.nix
+++ configuration.nix
@@ -344,11 +344,20 @@ in {
         # lsp
         cmp = {
           enable = true;
-          settings.sources = [
-            {name = "nvim_lsp";}
-            {name = "vsnip";}
-            {name = "cmp-buffer";}
-          ];
+          settings = {
+            sources = [
+              {name = "nvim_lsp";}
+              {name = "vsnip";}
+              {name = "cmp-buffer";}
+            ];
+            mapping = {
+              "<S-Tab>" = "cmp.mapping(cmp.mapping.select_prev_item(), {'i', 's'})";
+              "<Tab>" = "cmp.mapping(cmp.mapping.select_next_item(), {'i', 's'})";
+              "<CR>" = "cmp.mapping.confirm({ select = true })";
+              "<C-d>" = "cmp.mapping.scroll_docs(-4)";
+              "<C-f>" = "cmp.mapping.scroll_docs(4)";
+            };
+          };
         };
         cmp-vsnip.enable = true;
         cmp-buffer.enable = true;
