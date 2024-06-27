e1210ff87d154f47d50e9a5f0df4e78ebec41403
Zoe Soutter <zoe@soutter.com>
2024-06-27 09:54:12 +0100

    228 current  2024-06-27 09:54:07  24.11pre643242.2893f56de080  6.6.35                          *

--- configuration.nix
+++ configuration.nix
@@ -460,6 +460,8 @@ in {
             };
           };
         };
+        cmp-fuzzy-path.enable = true;
+        cmp-fuzzy-buffer.enable = true;
         cmp = {
           enable = true;
           settings = {
@@ -502,6 +504,60 @@ in {
         };
 
         # git
+        cmp-git = {
+          enable = true;
+          settings = {
+            github = {
+              issues = {
+                filter = "all";
+                format = ''
+                  function(_, issue)
+                    local icon = ({
+                      open = '',
+                      closed = '',
+                    })[string.lower(issue.state)]
+                    return string.format('%s #%d: %s', icon, issue.number, issue.title)
+                  end
+                '';
+                limit = 250;
+                sort_by = ''
+                  function(issue)
+                    local kind_rank = issue.pull_request and 1 or 0
+                    local state_rank = issue.state == 'open' and 0 or 1
+                    local age = os.difftime(os.time(), require('cmp_git.utils').parse_github_date(issue.updatedAt))
+                    return string.format('%d%d%010d', kind_rank, state_rank, age)
+                  end
+                '';
+                state = "all";
+              };
+            };
+            remotes = [
+              "upstream"
+              "origin"
+              "foo"
+            ];
+            trigger_actions = [
+              {
+                action = ''
+                  function(sources, trigger_char, callback, params, git_info)
+                    return sources.git:get_commits(callback, params, trigger_char)
+                  end
+                '';
+                debug_name = "git_commits";
+                trigger_character = ":";
+              }
+              {
+                action = ''
+                  function(sources, trigger_char, callback, params, git_info)
+                    return sources.github:get_issues(callback, git_info, trigger_char)
+                  end
+                '';
+                debug_name = "github_issues";
+                trigger_character = "#";
+              }
+            ];
+          };
+        };
         gitsigns = {
           enable = true;
           settings = {
