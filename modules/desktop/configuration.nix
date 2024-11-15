a8cadd7aa0f763fa072c6b638e9d342ba401f85a
= <=>
2024-11-15 19:49:07 +0000

    21 current  2024-11-10 23:28:27  24.11.20241004.bc947f5  6.6.53                          *

--- modules/desktop/configuration.nix
+++ modules/desktop/configuration.nix
@@ -6,4 +6,8 @@
   networking.hostName = "desktop"; # Define your hostname.
   services.logind.lidSwitchExternalPower = "ignore";
   services.octoprint.enable = true;
+  hardware.graphics = {
+    enable = true;
+    enable32Bit = true;
+  };
 }
