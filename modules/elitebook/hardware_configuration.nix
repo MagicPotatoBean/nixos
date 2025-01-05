212b826a52ea39e1e393b53d2d2b261723be5ace
Zoe Soutter <zoe@soutter.com>
2025-01-05 13:01:58 +0000

    4 current   2025-01-05 13:01:55  25.05.20241203.55d15ad     6.6.63                          *

--- modules/elitebook/hardware_configuration.nix
+++ modules/elitebook/hardware_configuration.nix
@@ -30,9 +30,9 @@
     options = ["fmask=0077" "dmask=0077"];
   };
 
-  swapDevices = [
-    {device = "/dev/disk/by-uuid/7db25a9c-242b-4f00-98fd-952ac30cdb1a";}
-  ];
+  # swapDevices = [
+  #   {device = "/dev/disk/by-uuid/7db25a9c-242b-4f00-98fd-952ac30cdb1a";}
+  # ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
